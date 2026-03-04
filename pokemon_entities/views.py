85c4e72e44a8559530b953a452071fe481ea84ca
Suba79 <coolzaken13@gmail.com>
2026-03-04 20:18:15 +0300

    add English and Japanese names to Pokemon model

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -94,6 +94,8 @@ def show_pokemon(request, pokemon_id):
     pokemon_data = {
         'pokemon_id': pokemon.id,
         'title_ru': pokemon.title,
+        'title_en': pokemon.title_en,
+        'title_jp': pokemon.title_jp,
         'img_url': request.build_absolute_uri(pokemon.image.url) if pokemon.image else None,
         'description': pokemon.description,
     }
