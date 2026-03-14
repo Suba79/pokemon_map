d02c5a364e007871714a776554bf06d3441ade84
Suba79 <coolzaken13@gmail.com>
2026-03-14 13:10:40 +0300

    remove unused import HttpResponseNotFound

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,6 +1,5 @@
 import folium
 
-from django.http import HttpResponseNotFound
 from django.shortcuts import render, get_object_or_404
 from django.utils.timezone import localtime
 from .models import Pokemon, PokemonEntity
